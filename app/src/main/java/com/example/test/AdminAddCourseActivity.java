e7e36de429fb2cb386140654822a96ae49225a20
Adam Badar <badaradam10@gmail.com>
2022-11-29 13:25:00 -0500

    Renamed and merged java files for AdminAddCourseActivity so that the firebase and UI are together in a single file

--- app/src/main/java/com/example/test/AdminAddCourseActivity.java
+++ app/src/main/java/com/example/test/AdminAddCourseActivity.java
@@ -1,14 +1,28 @@
 package com.example.test;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import com.example.test.databinding.ActivityMainBinding;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+import org.w3c.dom.Text;
 
 import java.util.ArrayList;
 import java.util.Collections;
@@ -21,14 +35,23 @@ public class AdminAddCourseActivity extends AppCompatActivity {
     private Button createCourseButton;
     //below is multiple dropdown
     TextView sessionButton;
+    TextView courseButton;
     boolean [] selectedSession;
+    boolean [] selectedCourse;
     ArrayList<Integer> sessionList = new ArrayList<>();
     String [] sessionArray = {"Fall", "Winter", "Summer"};
-
-    TextView courseButton;
-    boolean [] selectedCourse;
     ArrayList<Integer> courseList = new ArrayList<>();
     String [] courseArray = {"None", "CSCA48", "CSCA67", "CSCB36", "MATB41", "STAB52", "MATA31"};
+
+    private EditText courseName;
+    private EditText courseCode;
+    private TextView prerequisites;
+    private TextView sessionOfferings;
+    private FirebaseAuth auth;
+    private FirebaseDatabase db;
+    private DatabaseReference reference;
+
+    private ActivityMainBinding binding;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -49,10 +72,9 @@ public class AdminAddCourseActivity extends AppCompatActivity {
 
             }
         });*/
-        createCourseButton = (Button) findViewById(R.id.create_course);
-
+        createCourseButton = (Button) findViewById(R.id.createCourseBtn);
         //prerequisites
-        courseButton = findViewById(R.id.course_dropdown);
+        courseButton = findViewById(R.id.prereqs);
         selectedCourse = new boolean[courseArray.length];
 
         createCourseButton.setOnClickListener(new View.OnClickListener() {
@@ -116,7 +138,7 @@ public class AdminAddCourseActivity extends AppCompatActivity {
 
         });
         //sessions
-        sessionButton = findViewById(R.id.session_dropdown_multiple);
+        sessionButton = findViewById(R.id.sessionOffered);
         selectedSession = new boolean[sessionArray.length];
         sessionButton.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -170,8 +192,53 @@ public class AdminAddCourseActivity extends AppCompatActivity {
             }
         });
 
+        courseName = findViewById(R.id.courseName);
+        courseCode = findViewById(R.id.courseCode);
+        prerequisites = findViewById(R.id.prereqs);
+        sessionOfferings = findViewById(R.id.sessionOffered);
+
+
+        auth = FirebaseAuth.getInstance();
+        //progressBar =
+        String pattern = "[a-zA-z0-9._-]+@[a-z]+\\.+[a-z]+";
+        createCourseButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String txt_courseName = courseName.getText().toString().trim();
+                String txt_courseCode = courseCode.getText().toString().trim();
+                String txt_prerequisites = prerequisites.getText().toString().trim();
+                String txt_sessionOfferings = sessionOfferings.getText().toString().trim();
+
+                if ( TextUtils.isEmpty(txt_courseName) || TextUtils.isEmpty(txt_courseCode) || TextUtils.isEmpty((txt_sessionOfferings))) {
+                    Toast.makeText(AdminAddCourseActivity.this, "Empty Credentials", Toast.LENGTH_SHORT).show();
+                } else if (txt_courseCode.length() != 6) {
+                    Toast.makeText(AdminAddCourseActivity.this, "CourseCode too short", Toast.LENGTH_SHORT).show();
+                }
+                else {
+                    Course course = new Course(txt_courseName, txt_courseCode, prerequisites, txt_sessionOfferings);
+                    db = FirebaseDatabase.getInstance();
+                    reference = db.getReference("Courses");
+                    reference.child(txt_courseCode).setValue(course);
+                    //registerUser(txt_email, txt_password);
+                }
+            }
+        });
+
     }
 
-
+    private void CreateCourse(String courseName, String courseCode ) {
+        auth.createUserWithEmailAndPassword(email, password).addOnCompleteListener(AdminAddCourseActivity.this, new OnCompleteListener<AuthResult>() {
+            @Override
+            public void onComplete(@NonNull Task<AuthResult> task) {
+                if (task.isSuccessful()) {
+                    Toast.makeText(AdminAddCourseActivity.this, "Course creation successful!", Toast.LENGTH_SHORT).show();
+                    startActivity(new Intent(getApplicationContext(), AdminWelcomePage.class));
+                }
+                else {
+                    Toast.makeText(AdminAddCourseActivity.this, "Creation failed", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+    }
 
 }
