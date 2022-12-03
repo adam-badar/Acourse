098f89dbb260cb644b6d03c728c0246854eb3883
andrewaucie <aucieandrew@gmail.com>
2022-12-02 19:03:35 -0500

    edit

--- app/src/main/java/com/example/test/AdminCourse.java
+++ app/src/main/java/com/example/test/AdminCourse.java
@@ -1,5 +1,14 @@
 package com.example.test;
 
+import android.content.Intent;
+
+import androidx.annotation.NonNull;
+
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
 import java.util.ArrayList;
 
 public class AdminCourse {
@@ -19,7 +28,25 @@ public class AdminCourse {
         this.courseCode = courseCode;
         this.prerequisites = prerequisites;
         this.sessionOfferings =sessionOfferings;
-
+    }
+    public AdminCourse(String courseID) {
+        this.courseCode = courseID;
+        FirebaseDatabase.getInstance().getReference().child("Courses").addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                for (DataSnapshot snapshot: dataSnapshot.getChildren()) {
+                    AdminCourse admincourse = snapshot.getValue(AdminCourse.class);
+                    if (admincourse.courseCode.equals(courseID)) {
+                        courseName = admincourse.courseName;
+                        prerequisites = admincourse.prerequisites;
+                        sessionOfferings = admincourse.sessionOfferings;
+                    }
+                }
+            }
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+            }
+        });
     }
 
     public String getCourseName() {
